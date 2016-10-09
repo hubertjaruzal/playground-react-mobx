c19e46c14b90ec5ea61d46509af710cc97eaa13f
hubertjaruzal <hubertjaruzal@gmail.com>
2016-10-09 14:55:38 +0200

    Add filter

--- src/app.js
+++ src/app.js
@@ -7,27 +7,37 @@ class App extends Component {
     super(props);
 
     this.state = {
-      name: 'React-mobX',
-      list: this.props.todoStore.list
+      name: 'React-mobX'
     }
   }
 
   createNewTodo = (e) => {
     if(e.which === 13) {
-      this.props.todoStore.addTodo(e.target.value, this.state.list);
+      this.props.todoStore.addTodo(e.target.value, this.props.todoStore.list);
       e.target.value = '';
     }
+  }
+
+  filter = (e) => {
+    this.props.todoStore.filter = e.target.value;
   }
 
   render() {
     return (
       <div className='App'>
         <h1>{this.state.name}</h1>
-        <input onKeyPress={this.createNewTodo.bind(this)} />
+        <div>
+          <span>Filter: </span>
+          <input value={this.props.todoStore.filter} onChange={this.filter.bind(this)} />
+        </div>
+        <div>
+          <span>Add: </span>
+          <input onKeyPress={this.createNewTodo.bind(this)} />
+        </div>
         <div>
           <ul>
             {
-              this.state.list.map(todo => (
+              this.props.todoStore.filterTodoList.map(todo => (
                 <li key={todo.id}>
                   <span>{todo.name}</span>
                 </li>
--- src/store/todoStore.js
+++ src/store/todoStore.js
@@ -1,10 +1,16 @@
-import { autorun, observable } from 'mobx';
+import { autorun, observable, computed } from 'mobx';
 
 class TodoStore  {
   @observable list = [{id: 1, name: 'milk'}, {id: 2, name: 'eggs'}]
+  @observable filter = '';
 
   addTodo = (value, list) => {
     list.push({id: (list.length + 1), name: value})
+  }
+
+  @computed get filterTodoList() {
+    let matchesFilter = new RegExp(this.filter, 'i');
+    return this.list.filter((todo) => !this.filter || matchesFilter.test(todo.name));
   }
 }
 
